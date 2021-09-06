8b400ba2494b451cbb94128607043e467df6b265
Pranav <pranav4code@gmail.com>
2021-09-07 00:02:14 +0530

    Adding Jinja2 Template

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).parent
+print(BASE_DIR / "templates")
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory=str(BASE_DIR/"templates"))
+
+@app.get("/", response_class=HTMLResponse) # http GET -> JSON
+def home_view(request: Request):
+    #print(request)
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/") # http POST
+def home_detail_view():
+    return {"hello": "world"}
